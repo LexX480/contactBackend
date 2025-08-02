c0f99cd74f0f43540c69812878b7a40f6d992dc0
LexX480 <voidbaka47@gmail.com>
2025-08-02 14:58:19 +0545

    final ch

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,35 +5,30 @@ const jwt = require("jsonwebtoken");
 
 const registerUser = asyncHandler(async (req, res) => {
   const { username, email, password } = req.body;
+
   if (!username || !email || !password) {
-    res.status(400);
-    throw new Error("all fields are mandatory!");
+    return res.status(400).json({ message: "All fields are mandatory!" });
   }
+
   const userAvailable = await User.findOne({ email });
   if (userAvailable) {
-
-    res.status(400);
-    throw new Error("user already exists");
+    return res.status(400).json({ message: "User already exists" });
   }
-  //Hash password
+
   const hashedPassword = await bcrypt.hash(password, 10);
-  console.log("HashedPassword:", hashedPassword);
+
   const user = await User.create({
     username,
     email,
     password: hashedPassword,
   });
-  console.log(`User created ${user}`)
+
   if (user) {
-    res.status(201).json({ _id: user.id, email: user.email });
-
+    return res.status(201).json({ _id: user.id, email: user.email });
   } else {
-    res.status(400);
-    throw new Error("User data not valid")
+    return res.status(400).json({ message: "Invalid user data" });
   }
-  res.json({ message: "register the user" });
 });
-
 
 const loginUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
